9002fdbffe6936152d8037cd3ee95a92d8874e51
Raisadz <rdzhamtyrova@protonmail.com>
2022-11-29 22:15:47 +0000

    split lines

--- scripts/deployment.py
+++ scripts/deployment.py
@@ -18,15 +18,18 @@ os.makedirs(prod_deployment_path, exist_ok=True)
 
 def copy_to_deployment():
     logger.info(
-        f"Copy trainedmodel.pkl from {output_model_path} folder to {prod_deployment_path} folder"
+        f"Copy trainedmodel.pkl from {output_model_path} folder "
+        f"to {prod_deployment_path} folder"
     )
     shutil.copy2(f"{output_model_path}/trainedmodel.pkl", prod_deployment_path)
     logger.info(
-        f"Copy latestscore.txt from {output_model_path} folder to {prod_deployment_path} folder"
+        f"Copy latestscore.txt from {output_model_path} folder "
+        f"to {prod_deployment_path} folder"
     )
     shutil.copy2(f"{output_model_path}/latestscore.txt", prod_deployment_path)
     logger.info(
-        f"Copy ingestedfiles.txt from {dataset_csv_path} folder to {prod_deployment_path} folder"
+        f"Copy ingestedfiles.txt from {dataset_csv_path} folder "
+        f"to {prod_deployment_path} folder"
     )
     shutil.copy2(f"{dataset_csv_path}/ingestedfiles.txt", prod_deployment_path)
 
