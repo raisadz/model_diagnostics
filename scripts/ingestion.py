d88a16289b3d3d9094d13489ceb810b7383883fb
Raisadz <rdzhamtyrova@protonmail.com>
2022-11-29 18:58:14 +0000

    change ingestion script

--- scripts/ingestion.py
+++ scripts/ingestion.py
@@ -1,9 +1,9 @@
 import json
 import os
+import glob 
 
 import pandas as pd
 
-# Load config.json and get input and output paths
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -12,20 +12,16 @@ output_folder_path = config["output_folder_path"]
 
 os.makedirs(output_folder_path, exist_ok=True)
 
-
-# Function for data ingestion
 def merge_multiple_dataframe():
-    df_total = pd.DataFrame()
-    filename_list = []
-    # check for datasets, compile them together, and write to an output file
-    for filename in os.listdir(input_folder_path):
-        df = pd.read_csv(input_folder_path + "/" + filename)
-        df_total = df_total.append(df.reset_index(drop=True), ignore_index=True)
-        filename_list.append(filename)
-    df_dedup = df_total.drop_duplicates()
+    datasets = glob.glob(f'{input_folder_path}/*.csv')
+    df_total = pd.concat(map(pd.read_csv, datasets))
+    df_dedup = clean_data(df_total)
     df_dedup.to_csv(output_folder_path + "/finaldata.csv", index=False)
     with open(output_folder_path + "/ingestedfiles.txt", "w") as f:
-        f.write(str(filename_list))
+        f.write(str(os.listdir()))
+
+def clean_data(df):
+    return df.drop_duplicates()
 
 
 if __name__ == "__main__":
